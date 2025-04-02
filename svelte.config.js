fec544e8a5b4461c07755c54b10a37b8b529ddd1
UrbanoPrincess <202210285@gordoncollege.edu.ph>
2025-04-02 13:28:27 +0800

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,6 +11,7 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
+			
 		}),
 		alias: {
 			$lib: "src/lib"
